92f500321e40d076fc83e66052d9b6f78cbbeea4
Torsten Seemann <torsten.seemann@monash.edu>
2012-11-04 14:42:58 +1100

    Implemented yield (in bp)

--- fx-yield.c
+++ fx-yield.c
@@ -2,6 +2,16 @@
 
 int fx_yield(int argc, char* argv[])
 {
+  long yield=0;
+  for (int i=1; i < argc; i++) {
+    msg("Reading: %s", argv[i]);
+    SeqStream ss = SeqStream_open_single(argv[i]);
+    Seq s;
+    while (!Seq_read(&ss, &s)) {
+      yield += s.len;
+    }
+  }
+  printf("%ld\n", yield);
   return 0;
 }
 
