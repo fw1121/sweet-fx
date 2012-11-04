f952b70ca4784ed034d76642e40e45d9c26ceca6
Torsten Seemann <torsten.seemann@monash.edu>
2012-11-04 14:40:41 +1100

    Implement counting no. of sequences

--- fx-count.c
+++ fx-count.c
@@ -2,6 +2,16 @@
 
 int fx_count(int argc, char* argv[])
 {
+  long count=0;
+  for (int i=1; i < argc; i++) {
+    msg("Reading: %s", argv[i]);
+    SeqStream ss = SeqStream_open_single(argv[i]);
+    Seq s;
+    while (!Seq_read(&ss, &s)) {
+      count++;
+    }
+  }
+  printf("%ld\n", count);
   return 0;
 }
 
